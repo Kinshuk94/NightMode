77d71cfbf2711227f8786b8b90c5a1c93fe37956
kinshukkislay <kinshukkislay>
2020-09-23 14:42:54 +0200

    Created a night_mode UI for a normal web demo using the animate js and svg

---
+++ app.js
@@ -0,0 +1,41 @@
+const moonPath =
+    "M10.5 27.5C10.5 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 10.5 12.3122 10.5 27.5Z";
+
+const sunPath =
+    "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+
+const darkMode = document.querySelector("#darkMode");
+
+let toggle = false;
+
+darkMode.addEventListener('click', () => {
+    //set up timeline
+    const timeline = anime.timeline({
+        duration: 750,
+        easing: "easeOutExpo"
+    });
+    // Add different animations to the timeline
+    timeline.add({
+        targets: ".sun",
+        d: [
+            { value: toggle ? sunPath:moonPath }
+        ]
+    })
+    .add({
+        targets:"#darkMode",
+        rotate:320
+    },'-=350')
+    .add({
+        targets: "section",
+        backgroundColor: toggle ? 'rgb(255,255,255)':'rgb(22,22,22)',
+        color: toggle ? "rgb(22,22,22)":"rgb(255,255,255)"
+    },'-=700');
+
+    if(!toggle){
+        toggle = true;
+    }
+    else{
+        toggle = false; 
+    }
+
+});
